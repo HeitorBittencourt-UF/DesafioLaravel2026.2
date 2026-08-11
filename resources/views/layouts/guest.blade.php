383dd7e9b089346ff4d717ca1e173176a1e1af82
HeitorBittencourt <heitor.bittencourt@estudante.ufjf.br>
2026-08-11 19:45:25 -0300

    <feat>: Desenvolvimento do Guest estilizado

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,30 +1,34 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
-        <link rel="icon" type="image/png" href="{{ asset('assets/Logo-1.png') }}">
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/guest.css', 'resources/js/guest.js'])
-    </head>
-    <body class="guest-body">
-        <div >
-            <div>
-                <a href="/">
-                    <x-application-logo/>
-                </a>
-            </div>
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" type="image/png" href="{{ asset('assets/Logo-1.png') }}">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/guest.css', 'resources/js/guest.js'])
+</head>
+
+<body class="guest-body">
+    <div class="guest-container">
+        <a href="/" class="guest-logo-a">
+            <x-application-logo class="guest-logo" />
+        </a>
+
+
+        <div class="guest-card">
             <div>
                 {{ $slot }}
             </div>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
