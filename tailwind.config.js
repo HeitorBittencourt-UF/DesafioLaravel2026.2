d55c76febcbc3c607d5771009f713837a6118b05
HeitorBittencourt-UF <heitor.bittencourt@estudante.ufjf.br>
2026-08-26 01:19:34 -0300

    <feat>: tentativa de criacao login/register com slide

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,15 @@ export default {
                 montserrat: ['Montserrat', ...defaultTheme.fontFamily.sans],
                 league: ['"League Gothic"', ...defaultTheme.fontFamily.sans],
             },
+            keyframes: {
+                show: {
+                    '0%, 49.99%': { opacity: '0', zIndex: '1' },
+                    '50%, 100%': { opacity: '1', zIndex: '5' },
+                },
+            },
+            animation: {
+                show: 'show 0.6s step-end forwards',
+            }
         },
     },
 
