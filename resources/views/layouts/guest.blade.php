b3f112eb7adc76541c643c0f2ce01103aec23a8b
HeitorBittencourt-UF <heitor.bittencourt@estudante.ufjf.br>
2026-08-27 00:40:31 -0300

    <fix>:Retomada do antigo login

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -12,13 +12,10 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 <<<<<<< HEAD
-<<<<<<< HEAD
     <!-- Fonts: League Gothic & Montserrat -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=League+Gothic&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-=======
->>>>>>> parent of ffcc090 (<fix>: importando as fontes no guest.php)
 =======
 >>>>>>> parent of ffcc090 (<fix>: importando as fontes no guest.php)
 
