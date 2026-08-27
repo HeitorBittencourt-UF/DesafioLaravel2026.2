f962eed98df0a34120b8551d5f91963e25f391a6
HeitorBittencourt-UF <heitor.bittencourt@estudante.ufjf.br>
2026-08-27 01:52:26 -0300

    <feat>:confeccao inicial da navbar

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,14 +11,11 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-<<<<<<< HEAD
+
     <!-- Fonts: League Gothic & Montserrat -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=League+Gothic&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-=======
->>>>>>> parent of ffcc090 (<fix>: importando as fontes no guest.php)
-
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/css/guest.css', 'resources/js/guest.js'])
 </head>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,26 @@
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
         <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
             @if (Route::has('login'))
-                <nav class="flex items-center justify-end gap-4">
+
+                <nav class="navbar">
+                    <div class="navbar-logo">
+                        <x-application-logo></x-application-logo>
+                    </div>
+                    <ul class="navbar-links">
+                        <li><a href="#">Departamentos</a></li>
+                        <li><a href="#">Mais Curtidos</a></li>
+                        <li><a href="#">Mais Vendidos</a></li>
+                        <li><a href="#">Ofertas</a></li>
+                        <li><a href="#">Ajuda</a></li>
+                    </ul>
+                    <div class="navbar-icons">
+                        <x-user-icon/>
+                        <x-cart-icon/>
+                        <x-heart-icon/>
+                        <x-search-icon/>
+                    </div>
+                </nav>
+                <!-- <nav class="flex items-center justify-end gap-4">
                     @auth
                         <a
                             href="{{ url('/dashboard') }}"
@@ -46,7 +65,7 @@
                             </a>
                         @endif
                     @endauth
-                </nav>
+                </nav> -->
             @endif
         </header>
         <div class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
