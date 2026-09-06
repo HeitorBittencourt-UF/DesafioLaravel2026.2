7e5a6046f5cdea6fc54eeb080aef4647d646dd2c
HeitorBittencourt-UF <heitor.bittencourt@estudante.ufjf.br>
2026-09-06 18:13:49 -0300

    <fix>: troca de primary btn para nonpainted

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -19,9 +19,9 @@
         </div>
 
         <div class="flex justify-end mt-4">
-            <x-primary-button>
+            <x-nonpainted-area-button>
                 {{ __('Confirm') }}
-            </x-primary-button>
+            </x-nonpainted-area-button>
         </div>
     </form>
 </x-guest-layout>
--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -14,9 +14,9 @@
             @csrf
 
             <div>
-                <x-primary-button>
+                <x-nonpainted-area-button>
                     {{ __('Resend Verification Email') }}
-                </x-primary-button>
+                </x-nonpainted-area-button>
             </div>
         </form>
 
